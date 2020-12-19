5b63534d414ad0b871335a5c3edb1c315b08e9a6
INUZUKA Katsu <inuzuka0601@gmail.com>
2020-12-19 21:28:58 +0900

    mp3フォルダをDropboxから取り込むバッチプログラム。うっかりhtmlファイルに限定していたのでファイルの種類制限を解除した。

--- sync_mp3.rb
+++ sync_mp3.rb
@@ -7,19 +7,20 @@ Dir.chdir(__dir__)
 $access_token = "yqqyQxpAqngAAAAAAAAAAdYt0WfwqexuF4ahEUugRoOFxetIGwz1qVH09zlvfYBm"
 $client = DropboxApi::Client.new($access_token)
 
-def get_files_path_list(folder,files)
+def get_files_path_list(folder,files,kakuchoshi)
   adds = []
   list = $client.list_folder folder
   list.entries.each do |path|
     f = path.path_display
     if f[-5,5].match(/\./)
-      files << f if f.match(/html$/)
+      files << f if not kakuchoshi or File.extname(f)==kakuchoshi
     else
+      p f
       adds << f
     end
   end
   adds.each do |f|
-    get_files_path_list(f,files)
+    get_files_path_list(f,files,kakuchoshi)
   end
   files
 end
@@ -38,7 +39,13 @@ def get_from_dropbox(src_path,dest_path)
   end
 end
 
-list = get_files_path_list("/mp3",[])
+if ARGV[0]
+  kakuchoshi = ARGV[0]
+else
+  kakuchoshi = nil
+end
+list = get_files_path_list("/mp3",[],kakuchoshi)
+puts
 list.each do |path|
   p path
   FileUtils.mkdir_p(File.dirname('./mcc'+path),:mode => 666)
