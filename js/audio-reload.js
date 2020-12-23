9ffb071ecf146e34514e9cd72f2988d98b269164
INUZUKA Katsu <inuzuka0601@gmail.com>
2020-12-23 23:31:48 +0900

    audio-reload.jsのsetintervalがうまく動作していなかったので修正。

--- js/audio-reload.js
+++ js/audio-reload.js
@@ -1,12 +1,16 @@
+var id;
+var i=0;
 window.onload=function(){
   var elms = document.getElementsByTagName('audio');
-  var reload = function(){
-    Location.reload;
-    console.log('reloaded');
-    if(elms[elms.length-1].readyState>0){
-      //alert("ready!");
-      clearInterval(id);
-    }
+  var elm  = elms[elms.length-2]
+  id=setInterval(function(){reload(elm)},1500)
+}
+function reload(elm){
+  Location.reload;
+  console.log(i);
+  i++
+  console.log('readyState => ' + elm.readyState);
+  if(elm.readyState>0){
+    clearInterval(id);
   }
-  var id=setInterval(reload,1500)
 }
