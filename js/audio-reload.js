c10381a0a9adb17b02d50fac24e46ecf3c82d9af
INUZUKA Katsu <inuzuka0601@gmail.com>
2020-12-24 23:37:18 +0900

    audio-reload.jsの修正。これで一応完成のはず。 練習音源ページのcssファイルを追加。

--- js/audio-reload.js
+++ js/audio-reload.js
@@ -1,13 +1,29 @@
-var id;
-var i=0;
+
 window.onload=function(){
-  var elms = document.getElementsByTagName('audio');
-  var elm  = elms[elms.length-2]
-  setTimeout(function(){reload(elm)},1500);
-}
-function reload(elm){  
-  if(elm.readyState==0){
-    window.location.href = window.location.href;
-  }
+  var id   = setInterval(function(){reload(id)},1500);
 }
 
+function reload(id){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+      
+      if(xhr.readyState === 4 && xhr.status === 200) {
+        //var re = xhr.responseDocument;
+        var re = xhr.responseText;
+        var new_html = re.replace(/<.*?>/m,"")
+        //console.log( new_html );
+        document.innerHTML = new_html ;
+        var elms = document.getElementsByTagName('audio');
+        var elm  = elms[elms.length-2]
+        console.log(elm.readyState);
+        if(elm.readyState>0){
+          clearInterval(id);
+        }
+      }
+  
+  }
+  xhr.open('GET', document.location);
+  xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+  xhr.send( null );
+}
+ 
